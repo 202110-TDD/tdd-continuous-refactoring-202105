62f3dd2ff2685dd02097369ebf4160e3cadbd9c8
Joey Chen <jay.hatelove@gmail.com>
2021-10-02 23:26:41 +0800

    [refactor] clean up code, and inline temp variable: budget

--- test/budgetService.js
+++ test/budgetService.js
@@ -5,21 +5,14 @@ import {Period} from "../src/period";
 export class BudgetService {
 
     query(start, end) {
-        // let sum = 0;
         let period = new Period(dayjs(start), dayjs(end));
 
         return this.getAll()
             .map(b => {
-                let budget = Budget.from(b);
-                return budget.overlappingAmount(period);
+                return Budget.from(b).overlappingAmount(period);
             })
             .reduce((x, y) => x + y, 0);
 
-        // .forEach(b => {
-        //     let budget = Budget.from(b);
-        //     sum += budget.overlappingAmount(period);
-        // });
-        // return sum;
     }
 
 }
