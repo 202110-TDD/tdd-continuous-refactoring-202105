34a23018c1b898b47a3caa095ca8e7aa63d120dd
Joey Chen <jay.hatelove@gmail.com>
2021-10-02 23:16:24 +0800

    [refactor] move duplicated code of overlappingEnd out of if/else block

--- src/period.js
+++ src/period.js
@@ -9,22 +9,21 @@ export class Period {
     }
 
     overlappingDays(budget) {
-        let overlappingEnd;
+        let overlappingEnd = budget.lastDay().isBefore(this.endDay)
+            ? budget.lastDay()
+            : this.endDay;
         let overlappingStart;
         if (budget.firstDay().isSame(this.startDay, "month")) {
-            overlappingEnd = budget.lastDay().isBefore(this.endDay)
-                ? budget.lastDay()
-                : this.endDay;
             overlappingStart = this.startDay;
         } else if (budget.lastDay().isSame(this.endDay, "month")) {
-            overlappingEnd = budget.lastDay().isBefore(this.endDay)
-                ? budget.lastDay()
-                : this.endDay;
+            // overlappingEnd = budget.lastDay().isBefore(this.endDay)
+            //     ? budget.lastDay()
+            //     : this.endDay;
             overlappingStart = budget.firstDay();
         } else {
-            overlappingEnd = budget.lastDay().isBefore(this.endDay)
-                ? budget.lastDay()
-                : this.endDay;
+            // overlappingEnd = budget.lastDay().isBefore(this.endDay)
+            //     ? budget.lastDay()
+            //     : this.endDay;
             overlappingStart = budget.firstDay();
         }
         return overlappingEnd.diff(overlappingStart, "day") + 1;
