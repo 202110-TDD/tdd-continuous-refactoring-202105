86d8fb34eec3baf0334324aff862c21cdef6b34d
Joey Chen <jay.hatelove@gmail.com>
2021-10-02 23:11:26 +0800

    [refactor] move overlappingDays() to Period class

--- src/period.js
+++ src/period.js
@@ -8,4 +8,19 @@ export class Period {
 
     }
 
+    overlappingDays(budget) {
+        let overlappingEnd;
+        let overlappingStart;
+        if (budget.firstDay().isSame(this.startDay, "month")) {
+            overlappingEnd = budget.lastDay();
+            overlappingStart = this.startDay;
+        } else if (budget.lastDay().isSame(this.endDay, "month")) {
+            overlappingEnd = this.endDay;
+            overlappingStart = budget.firstDay();
+        } else {
+            overlappingEnd = budget.lastDay();
+            overlappingStart = budget.firstDay();
+        }
+        return overlappingEnd.diff(overlappingStart, "day") + 1;
+    }
 }
