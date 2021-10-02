57f70009662c3895a5844668b40a0e00e5302ed9
Joey Chen <jay.hatelove@gmail.com>
2021-10-02 23:18:59 +0800

    [refactor] move period initialization out of for loop

--- test/budgetService.js
+++ test/budgetService.js
@@ -14,12 +14,12 @@ export class BudgetService {
             return (this.getAll()?.find(element => element.yearMonth === startDay.format("YYYYMM"))?.amount || 0) / startDay.daysInMonth() * (endDay.diff(startDay, "day") + 1);
         }
         let sum = 0;
+        let period = new Period(startDay, endDay);
         for (let currentMonth = startDay.startOf("month");
              currentMonth.isBefore(endDay.add(1, 'month').startOf('month'));
              currentMonth = currentMonth.add(1, "month")) {
             let budget = Budget.from(this.getAll()?.find(element => element.yearMonth === currentMonth.format("YYYYMM")));
             if (budget) {
-                let period = new Period(startDay, endDay);
                 sum += budget.dailyAmount() * period.overlappingDays(budget);
             }
         }
