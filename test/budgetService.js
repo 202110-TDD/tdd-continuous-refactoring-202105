2836e40a46498167525220a1c3ed45344207965b
Joey Chen <jay.hatelove@gmail.com>
2021-10-02 22:38:48 +0800

    [refactor] remove unused null check of budget?.amount

--- test/budgetService.js
+++ test/budgetService.js
@@ -18,11 +18,11 @@ export class BudgetService {
             let budget = this.getAll()?.find(element => element.yearMonth === month);
             if (budget) {
                 if (currentMonth.isSame(startDay, "month")) {
-                    sum += (budget?.amount || 0) / currentMonth.daysInMonth() * (startDay.endOf("month").diff(startDay, "day") + 1);
+                    sum += (budget.amount) / currentMonth.daysInMonth() * (startDay.endOf("month").diff(startDay, "day") + 1);
                 } else if (currentMonth.isSame(endDay, "month")) {
-                    sum += (budget?.amount || 0) / currentMonth.daysInMonth() * (endDay.diff(endDay.startOf("month"), "day") + 1);
+                    sum += (budget.amount) / currentMonth.daysInMonth() * (endDay.diff(endDay.startOf("month"), "day") + 1);
                 } else {
-                    sum += budget?.amount || 0;
+                    sum += budget.amount;
                 }
             }
         }
